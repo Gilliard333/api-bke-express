d9f2ecc86bf42191b91026c5287638c379cb6ad0
Gilliard Oliveira <gilliard.oliveira@aluno.ifsp.edu.br>
2024-07-31 22:17:15 -0300

    add import type module and hello world express

--- README.md
+++ README.md
@@ -4,3 +4,16 @@
 
 ```
 node -v #verifica versão do node
+npm -v #verifica versão do npm
+npm init #cria o package json
+
+```
+
+### Git
+
+```
+git config --global user.name "Seu Nome"
+git config --global user.email "seuemail@gmail.com"
+git remote add origin https://github.com/seugithub/seuprojeto.git
+
+```
--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log('Olá Mundo!')
+//const express = require('express')
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!, kkkkkkk')
+})
+
+app.get('/user', (req, res) => {
+  res.json({nome:"Gilliard", email: "gilliard.oliveira@email.com"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
