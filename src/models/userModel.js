51f36aac6b7e67d8c757a1a321ff744e57847c89
Gilliard Oliveira <gilliard.oliveira@aluno.ifsp.edu.br>
2024-08-14 19:28:24 -0300

    user, userModel , userRouter,

---
+++ src/models/userModel.js
@@ -0,0 +1,20 @@
+const getAll = () => {
+    return [
+        {
+            id: 1,
+            name: "Gilliard",
+            email: "gilliard@gmail.com"
+        },
+        {
+            id: 2,
+            name: "Maria",
+            email: "maria@gmail.com"
+        }
+    ]
+}
+
+const create = () => {
+
+}
+
+export default {getAll, create}
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -5,19 +5,15 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.json({message: 'Rota Get /product'})
   })
-  
 router.post('/', (req, res) => {
     res.json({message: 'Rota Post /product'})
   })
-  
 router.put('/', (req, res) => {
     res.json({message: 'Rota Put /product'})
   })
-  
 router.patch('/', (req, res) => {
     res.json({message: 'Rota Patch /product'})
   })
-  
 router.delete('/', (req, res) => {
     res.json({message: 'Rota Delete /product'})
   })
