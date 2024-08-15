4ef7715339dcdd1a0f2ba1f209a62c9188d6603b
Gilliard Oliveira <gilliard.oliveira@aluno.ifsp.edu.br>
2024-08-14 22:18:59 -0300

    schema Prisma

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,20 +1,31 @@
-const getAll = () => {
-    return [
-        {
-            id: 1,
-            name: "Gilliard",
-            email: "gilliard@gmail.com"
-        },
-        {
-            id: 2,
-            name: "Maria",
-            email: "maria@gmail.com"
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const getAll = async () => {
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true,           
         }
-    ]
+    });
+    return users
+} 
+//    return [
+//        {
+//            id: 1,
+//            name: "Gilliard",
+//            email: "gilliard@gmail.com"
+//        },
+//        {
+//            id: 2,
+//            name: "Maria",
+//            email: "maria@gmail.com"
+//       }
+//    ]
+//}
+
+export const create = () => {
+
 }
-
-const create = () => {
-
-}
-
-export default {getAll, create}
